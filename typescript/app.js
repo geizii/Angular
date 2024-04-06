4cec9357a0e3af80fc5ec5fa617e80858d81a5e0
geizii <4663072@alunouninter.com>
2024-04-05 21:04:03 -0300

    primeiras aulas curso angular

---
+++ typescript/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let ola = (nome) => {
+    console.log("Olá" + nome);
+};
+ola('Geiziane');
---
+++ typescript/app.ts
@@ -0,0 +1,15 @@
+/* let ola = (nome:string) => {
+    console.log("Olá" + nome)
+} 
+
+ola('Geiziane')
+*/
+
+
+let mensagem: string = 'Seja bem vindo!!'
+let temporadasFriends: number = 10
+let estudandoAngular: Boolean= true
+let listaDeFrutas: Array<string> = ['Banana', 'Uva', 'Melão']
+let notaDasProvas: Array<number>= [7.5, 8, 9]
+
+mensagem = 'Oi'
